69b34e049fac308264b71a6f396a7792e8e45531
grumps <max@ofmax.li>
2015-07-27 13:06:24 -0700

    Lets make it more complex.

--- madlib.py
+++ madlib.py
@@ -1,24 +1,32 @@
 # Madlib program via Python3 Day 1 of bootcamp
 
+def get_input(question):
+    return input(question)
 
-# Get first lib
-lib = input("Who is this going to be about? ")
+def add_to_story(lib):
+    # this is probably ugly :-)
+    global the_full_monty
+    the_full_monty += lib
 
-fullstory = lib + " has joined the search for "
+the_full_monty = "\n\n"
+fullstory = add_to_story(get_input("Who is this going to be about? "))
+fullstory = add_to_story(" has joined the search for ")
 
 # get 2nd lib
-lib = input("How many are missing? ")
-fullstory += lib + "teenage boys who disappeared"
+add_to_story(get_input("How many are missing? "))
+add_to_story(" teenage boys who disappeared ")
+add_to_story(" off the coast of ")
+add_to_story(get_input("What coast are they off of? "))
+add_to_story(" during a fishing trip Friday adding the USS ")
 
-# get 3rd lib
-lib = input("where did they disappear from? ")
-fullstory += " off the coast of " + lib + "during a fishing trip Friday, adding the USS "
+# we're gonna use boat_name to determine the size of the area.
+boat_name = get_input("What's the name of the boat? ")
+add_to_story(boat_name)
+add_to_story(" to a Coast Guard search team that has covered about ")
 
-# get 4th lib
-lib = input("What's the name of the boat? ")
-fullstory += lib + "to a Coast Guard search team that has covered about "
-howfar = str(len(lib) * 20000)
+# use boat name to determine our search area.
+add_to_story(str(len(boat_name) * 20000))
+add_to_story(" square nautical miles — by air and sea — in the hunt for Austin Stephanos and Perry Cohen.")
 
-fullstory += howfar + " square nautical miles — by air and sea — in the hunt for Austin Stephanos and Perry Cohen."
-
-print(fullstory)
+add_to_story("\n\n")
+print(the_full_monty)
