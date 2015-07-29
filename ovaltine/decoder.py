5caa8924646a96cc09e76fe011d8f7aa9fe5a086
grumps <max@ofmax.li>
2015-07-29 12:08:51 -0700

    add  encoding and decoding. Add docstrings.

--- ovaltine/decoder.py
+++ ovaltine/decoder.py
@@ -21,18 +21,48 @@ class Decoder():
         self.process()
 
     def process(self):
+        """
+        Flow control function
+        """
         secret_message = self.read_secret(self.filename)
         not_so_secret_message = self.decode(secret_message)
         print(not_so_secret_message)
 
     def read_secret(self, filename):
-        with open(filename, 'r') as f:
-            return f.read()
+        """
+        filename: name of file in the current working dirtectory.
+        returns string of content from given filename.
+        """
+        try:
+            with open(filename, 'r') as f:
+                secret_file = f.read()
+            return secret_file
+        except IOError:
+            print("input file: {} not found.".format(self.filename))
+            exit()
 
     def decode(self, message):
+        """
+        decode funcation automagically attemtps to tell if the message is
+            encoded with the `-s` option.
+        message: encoded message
+        """
         decoded_message = ""
-        for ordinal in message.split(sep=" "):
-            decoded_message += chr(int(ordinal))
+        try:
+            for ordinal in message.split(sep=" "):
+                if len(ordinal) <= 3:
+                    # We check for simple ordinal encoding
+                    continue
+                elif len(ordinal) == 8:
+                    num_ordinal_digits_with_offset = int(ordinal[:1]) + 3
+                    ordinal = ordinal[3:num_ordinal_digits_with_offset]
+                else:
+                    # Someone is throwing dirty things at us.
+                    raise ValueError
+                decoded_message += chr(int(ordinal))
+        except ValueError:
+            print("Unkown message type.")
+            exit()
         return decoded_message
 
 if __name__ == '__main__':
--- ovaltine/encoder.py
+++ ovaltine/encoder.py
@@ -3,7 +3,7 @@
 encoder module
 """
 import argparse
-
+from random import randint
 
 class Encoder():
 
@@ -12,7 +12,6 @@ class Encoder():
     """
 
     def __init__(self):
-
         parser = argparse.ArgumentParser(description="Encode a message.")
         parser.add_argument('-m', '--message',
                             help='messeage to encrypt',
@@ -20,9 +19,13 @@ class Encoder():
         parser.add_argument('-o', '--output',
                             help='output file for encrypted message',
                             default='secret.txt')
-
+        parser.add_argument('-s', '--secure',
+                            help='make message more secure',
+                            action="store_true",
+                            default=False)
         args = parser.parse_args()
         message = args.message
+        self.secure = args.secure
         self.filename = args.output
         self.process(message)
 
@@ -34,13 +37,33 @@ class Encoder():
     def make_secret(self, secret_message, filename):
         with open(filename, 'w') as f:
             f.write(secret_message)
-            return f.name
 
     def transform(self, message):
         ordinal_message = ""
         for char in message:
-            ordinal_message += str(ord(char)) + " "
+            ordinal_char = str(ord(char))
+            if self.secure:
+                len_ordinal_char = len(ordinal_char)
+                str_len_ordinal_char = str(len_ordinal_char)
+                ordinal_message += str_len_ordinal_char + self.get_fluff(3) + \
+                    ordinal_char + \
+                    self.get_fluff(len_ordinal_char) + " "
+
+            elif not self.secure:
+                ordinal_message += ordinal_char + " "
         return ordinal_message[:-1]
+
+    def get_fluff(self, num_digits):
+        """
+        num_digits: number of digits for ordinal, given 2 or 3
+        returns: returns random number as string opposite of num_digits
+        """
+        if num_digits == 2:
+            return str(randint(99, 999))
+        elif num_digits == 3:
+            return str(randint(31, 99))
+        else:
+            raise ValueError
 
 if __name__ == '__main__':
     Encoder()
