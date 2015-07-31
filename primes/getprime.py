ff1ac0a168af28102a89f4f77100f30674ef3460
grumps <max@ofmax.li>
2015-07-31 13:51:57 -0700

    fix bug.

--- primes/getprime.py
+++ primes/getprime.py
@@ -4,27 +4,31 @@ from sys import argv
 # take input
 
 
-def find_prime(maxvalue, primes):
+def find_prime(maxvalue):
     primes = [2, 3, 5]
     for num in range(7, int(maxvalue), 6):
-        if is_prime(num):
+        if is_prime(num, primes):
             primes.append(num)
-        if is_prime(num + 4) and (num + 4) < maxvalue:
+        if is_prime(num + 4, primes) and (num + 4) < maxvalue:
             primes.append(num + 4)
     return primes
 
 
 # is prime
 def is_prime(num, primes):
+    """
+    num: number to check if it's prime.
+    primes: 
+    """
     if 0 not in [num % x for x in primes]:
         return True
     else:
         return False
 
 
-def write(rows):
+def write_to_file(rows):
     """
-    Takes a list, and writes to disk, as a row.
+    Takes a list, and writes to disk, as a row for each item in list.
     """
     f = open('primes.txt', 'w')
     for row in rows:
@@ -41,7 +45,7 @@ def main():
         exit()
     elif len(argv) == 2:
         primes = find_prime(int(argv[1]))
-        write(primes)
+        write_to_file(primes)
     else:
         print("Unknown error has occured.")
 
