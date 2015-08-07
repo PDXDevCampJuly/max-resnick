ec82f08f34d7a2a49199d21fb2b38fe461da37db
grumps <max@ofmax.li>
2015-08-07 12:23:58 -0700

    cleanup, remove is_hidden as arg.

--- blackjack/card.py
+++ blackjack/card.py
@@ -3,21 +3,16 @@ class Card:
     Class of cards
     """
 
-    def __init__(self, face_value, real_value, suit, color, is_hidden=False):
-    #    self.face_value_and_real_value, self.real_value = _unpack_card(face_value_and_real_value)
+    def __init__(self, face_value, real_value, suit, color):
         self.suit = suit
         self.color = color
-        self.is_hidden = is_hidden
+        self.is_hidden = True
         self.real_value = real_value
         self.face_value = face_value
 
 
     def __repr__(self):
-        if self.is_hidden:
-            pretty_card = "This card is hidden"
-        else:
-            pretty_card = str(self.face_value) + " of " + str(self.suit)
-        return pretty_card
+        return self.string
 
     def __str__(self):
         if self.is_hidden:
