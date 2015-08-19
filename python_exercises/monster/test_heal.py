b1888d8ece12bbb2d02aa8f1f8ee15f463adf21f
grumps <max@ofmax.li>
2015-08-19 11:20:30 -0700

    test for ideal input

--- python_exercises/monster/test_heal.py
+++ python_exercises/monster/test_heal.py
@@ -15,5 +15,11 @@ class MyTestCase(unittest.TestCase):
         """test initialized health value"""
         self.assertEqual(10, self.test_monster.health)
 
+    def test_ideal_add_health(self):
+        """test adding health, with input that will keep at 10 and below"""
+        # we have an unhealthy monster
+        self.test_monster.health = 2
+        self.assertEqual(10, self.test_monster.heal(8))
+
 if __name__ == '__main__':
     unittest.main()
