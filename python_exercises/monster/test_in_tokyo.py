1b4f68142555fd268783d00b9b04fe117a153f25
grumps <max@ofmax.li>
2015-08-19 11:10:37 -0700

    added ideal, in tokyo status check

--- python_exercises/monster/test_in_tokyo.py
+++ python_exercises/monster/test_in_tokyo.py
@@ -15,6 +15,14 @@ class InTokyoTest(unittest.TestCase):
         """check if we initialize correctly"""
         self.assertEqual("Out of Tokyo", self.test_monster.status)
 
+    def test_is_not_in_tokyo(self):
+        """return False if we're not in tokyo"""
+        self.assertFalse(self.test_monster.in_tokyo())
+
+    def test_is_in_tokyo(self):
+        """check if return is true, when status is 'In Tokyo'"""
+        self.test_monster.status = "In Tokyo"
+        self.assertTrue(self.test_monster.in_tokyo())
 
 if __name__ == '__main__':
     unittest.main()
