20a2c0ae7e70eb76ad8a477b2e3cc325eb3d5fc6
grumps <max@ofmax.li>
2015-08-28 10:53:48 -0700

    REGEX ALL THE THINGS :japanese_goblin:

--- JavaScript_Exercises/javapic/js/join.js
+++ JavaScript_Exercises/javapic/js/join.js
@@ -10,24 +10,46 @@
 
 
     function validate(e) {
-        /*
-         * @description validates form
-         */
+    /*
+     * @description validates form
+     */
     this.fieldsToValidate = e;
     this.validation = null;
     if (this.fieldsToValidate.getAttribute('name') === 'name') {
-        var name = /[a-z]*/.test(this.fieldsToValidate);
+        // REGEX
+        var name = /^[a-zA-Z]+[a-zA-Z ]+[a-zA-Z]$/
+            .test(this.fieldsToValidate.value);
+        // Going to explicitly test for true and false. What if it's all janky?
         if (!name) {
             this.validation = false;
+        } else if (name) {
+            this.validation = true;
         }
         this.validation = true;
     } else if (this.fieldsToValidate.getAttribute('name') === 'email') {
-        var email = /^[a-z]+[\w\._+]+@([^\.]\w*+\.)*+[a-z]*/.test(this.fieldsToValidate.text);
+        /*
+         * ^[a-zA-Z] - email usernamesstart w/ alpha only
+         * [\w\._+] any alphanumeric but also "." "_" "+" are valid specials for usernames.
+         * must be a @
+         * any number of domains, subdomains as long as we end with a char.
+         */
+        var email = /^[a-zA-Z]+[\w\._+]+@([^\.]\w*\.)*[a-zA-Z]*[a-zA-Z]$/
+            .test(this.fieldsToValidate.value);
         if (!email) {
             this.validation = false;
+        } else if (email) {
+            this.validation = true;
         }
-        this.validation =  true;
 
+    } else if (this.fieldsToValidate.getAttribute('name') === 'username') {
+        // REGEX
+        var username = /^[a-zA-Z]+[\w\.][a-zA-Z0-9]$/
+            .test(this.fieldsToValidate.value);
+        if (!username) {
+            this.validation = false;
+        } else if (username){
+            this.validation = true;
+        }
     }
     console.log(this.validation);
 }
