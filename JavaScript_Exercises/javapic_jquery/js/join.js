51e5132d3e827edc6158e4565fba03d4dd4f5db0
grumps <max@ofmax.li>
2015-09-02 16:06:28 -0700

    working validation.

--- JavaScript_Exercises/javapic_jquery/js/join.js
+++ JavaScript_Exercises/javapic_jquery/js/join.js
@@ -2,44 +2,83 @@
  * Created by Maxwell J. Resnick on 9/1/15.
  */
 // Overkill for this assignment
-var javapicImages = {
-       /*
-        * javapicImages
-        * @description  First we need to select some images of the 60
-        * @returns [array] 6 image file paths
-        * Using an IIFY for fun but also we only need this list once, per page load.
-        */
-        filePrefix: "url(images/pdxcg_",
-        filePostfix: ".jpg)",
-        randomImages: function () {
-            for (var i = 0; i < 7; i++) {
-                this._randomImages = [];
-                // we go up to 60, but add 1 in the end to account for 0.
-                this.num = Math.floor(Math.random() * 60) + 1;
-                // we add a "0" but also just cast everything to string
-                if (this.num < 10) {
-                    this.num = "0" + this.num.toString();
-                } else {
-                    this.num = this.num.toString();
-                }
-                this.fullPath = filePrefix + this.num + filePostfix;
-                this.randomImages.push(this.fullPath);
+var $jQ = jQuery.noConflict();
+
+var form = {
+    name: function (name) {
+            this.isValid = /^([a-zA-Z]+)$/
+                    .test(name);
+            this.error = "Your name must be chars only.";
+            if (!this.isValid) {
+                error = this.error;
             }
-            return this._randomImages;
-        },
-        allImages: function() {
-                this.images = [];
-                for (var i = 0; i < 60; i++) {
-                    this.num = i+1;
-                    // we add a "0" but also just cast everything to string
-                    if (this.num < 10) {
-                        this.num = "0" + this.num.toString();
-                    } else {
-                        this.num = this.num.toString();
-                    }
-                    this.fullPath = this.filePrefix + this.num + this.filePostfix;
-                    this.images.push(this.fullPath);
-                    }
-                    return this.images;
-        },
+            return this.isValid;
+          },
+    username: function (username) {
+            this.isValid = /^[a-zA-Z]+[\w\.][a-zA-Z0-9]+$/
+                    .test(username);
+            this.error = "Your name must be number, chars, and _ only.";
+            if (!this.isValid) {
+                error = this.error;
+            }
+            return this.isValid;
+          },
+    email: function (email) {
+            this.isValid = /^[a-zA-Z]+[\w\._+]+@([^\.]\w*\.)*[a-zA-Z]*[a-zA-Z]$/
+                    .test(email);
+            this.error = "You must have a valid email address.";
+            if (!this.isValid) {
+                error = this.error;
+            }
+            return this.isValid;
+          },
+    error: null,
 };
+
+
+function formHandler($e) {
+    this.form = new form;
+    $e.each(function () {
+            if (!form[this.name](this.value)) {
+                $jQ(this).after(function () {
+                        return '<div class="error">' + form.error + '</div>';
+                    });
+            } else {
+                $jQ(this).next('.error').remove();
+            }
+        });
+};
+
+(function () {
+    /*
+     * @description make my error class.
+     */
+
+    this.sheet = document.styleSheets[0];
+    this.errorStyle = (".error {" +
+                       "background-color: tomato;" +
+                       "color: #fff;" +
+                       "size: .75em;" +
+                       "padding: 10px;" +
+                       "margin: 10px;" +
+                       "border-radius: 5px; }");
+    this.sheet.insertRule(this.errorStyle, 0);
+}());
+$jQ(function () {
+    /*
+     * @description event bindings.
+     */
+    $jQ('#signup').on({
+        'submit': function(e){
+            e.preventDefault();
+            var toCheck = $jQ(e.target).children('input').not("#submit");
+            if(formHandler(toCheck)) {
+                registerUser();
+            };
+        },
+        'change': function(e){
+            var toCheck = $jQ(e.target);
+            formHandler(toCheck);
+        }
+    });
+}());
