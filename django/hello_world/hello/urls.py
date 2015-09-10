4f10a5bb8d57c386d4ea4b04b4fb1b49428ee454
grumps <max@ofmax.li>
2015-09-10 16:34:30 -0700

    sample django app.

---
+++ django/hello_world/hello/urls.py
@@ -0,0 +1,12 @@
+__author__ = 'Maxwell J. Resnick'
+
+from django.conf.urls import include, url
+from hello import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'hello_world.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.index, name='index'),
+    url('corgi.html', views.corgi, name='corgi'),
+]
