e3ac6fc874a015957b05a5f63a5c7ce9a58e5752
grumps <max@ofmax.li>
2015-09-11 14:07:35 -0700

    integrated javapic.

--- django/html_portfolio/javapic/views.py
+++ django/html_portfolio/javapic/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 
 def javapic(request):
     """
-    JavaPic homepages.
+    JavaPic homepages
     """
     return render(request, 'javapic.html')
+
+def join_javapic(request):
+    """
+    JavaPic form
+    """
+    return render(request, 'join.html')
+
+def gallery_javapic(request):
+    """
+    Javapic gallery
+    """
+    return render(request, 'gallery.html')
