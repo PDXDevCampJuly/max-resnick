20bb68c37c61b97ff993c75c45181a2678ae2810
grumps <max@ofmax.li>
2015-09-11 11:41:35 -0700

    adding javpic

--- django/html_portfolio/html_portfolio/urls.py
+++ django/html_portfolio/html_portfolio/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from javapic import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'html_portfolio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^javapic/$', views.javapic, name='javapichome'),
 ]
